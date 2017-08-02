d16c50a62f801caec7c6f5685917f07b9f17a179
fangqing <fangqing@114study.com>
2017-08-02 15:31:49 +0800

    'update'

--- system/library/db/build.js
+++ system/library/db/build.js
@@ -5,10 +5,12 @@ var execute = require('./execute');
 
 if( 'options' in _config){
     if( 'database' in _config.options){
-        // 配置为全局DB配置文件
-        global.db = { };
+        // DB配置
+        global._config.db = {};
         global._config.db.config = _config.options.database;
         global._config.db.conn = mysql().conn;
+        // 配置为全局DB配置文件
+        global.db = { };
         // 查询类构造
         global.db.query = new query();
         // 执行类构造
